f5e5d66dbe5b3f29ccc81836c774f682f136d1e3
Kshitij-Agrawal-MKG <kshitijagrawal53@gmail.com>
2024-04-22 11:05:54 +0530

    replace src

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,23 +8,23 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Kshitij Agrawal </span>
+            from <span className="purple"> Jaipur, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently learning and exploring the world of Artificial Intelligence.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have pursuing my Bachelor of Technology (B.Tech) in Information Technology at
+            SKIT, Jaipur.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Solve real life problem
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Explore new things
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Kshitij</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,29 +2,35 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
+  SiGithub,
+  SiGit,
   SiVercel,
-  SiMacos,
+  SiAnaconda,
+  SiJupyter,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <div style={{ fontSize: "25%" }}>Visual Studio Code</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <SiAnaconda />
+        <div style={{ fontSize: "25%" }}>Anaconda</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+        <SiGithub />
+        <div style={{ fontSize: "25%" }}>Github</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+        <div style={{ fontSize: "25%" }}>Vercel</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJupyter />
+        <div style={{ fontSize: "25%" }}>Jupyter Notebook</div>
       </Col>
     </Row>
   );
