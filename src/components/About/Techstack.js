d1f94326078fe83172e73ac554ccb3e775882b0b
Kshitij-Agrawal-MKG <kshitijagrawal53@gmail.com>
2024-04-23 18:46:58 +0530

    update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { IoIosChatboxes } from "react-icons/io";
-import { FaBrain } from "react-icons/fa6";
 import {
   DiReact,
   DiMongodb,
