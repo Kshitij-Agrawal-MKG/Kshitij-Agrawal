6b9ab91452bf0e86bb2c2829c0e5937d6da6de9a
Kshitij-Agrawal-MKG <kshitijagrawal53@gmail.com>
2024-04-26 21:01:48 +0530

    project images upload

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,6 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -26,9 +25,8 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Trackem"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              description="Trackem AI based car bike tracking system  uses YOLO v8 and OpenCV for real-time vehicle detection and license plate recognition, integrating with criminal databases. It sends alerts via Gmail and Telegram APIs, enhancing surveillance efficiency"
+              ghLink="https://github.com/Kshitij-Agrawal-MKG/"             
             />
           </Col>
 
@@ -38,8 +36,7 @@ function Projects() {
               isBlog={false}
               title="Automatic Image Captioning"
               description="This project aims to develop a Deep Learning application that merges cutting-edge techniques in computer vision and natural language processing. The system takes images as inputs and generates human-readable descriptions."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/Kshitij-Agrawal-MKG/"
             />
           </Col>
 
@@ -49,8 +46,7 @@ function Projects() {
               isBlog={false}
               title="Gemini"
               description="Welcome to the Gemini repository! This project introduces a cutting-edge Streamlit web application powered by Google's Generative AI model, Gemini Pro. With Gemini, users can effortlessly obtain insightful answers to their questions through an interactive interface."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/Kshitij-Agrawal-MKG/"             
             />
           </Col>
 
@@ -60,8 +56,7 @@ function Projects() {
               isBlog={false}
               title="Hand Gesture Recognition"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/Kshitij-Agrawal-MKG/"
             />
           </Col>
 
@@ -71,8 +66,7 @@ function Projects() {
               isBlog={false}
               title="Spam Email Detection"
               description="Spam detection is crucial in filtering out unwanted or unsolicited messages, such as emails or text messages. It involves employing machine learning algorithms to analyze message content and identify patterns associated with spam. This repository offers a Python script utilizing various machine learning models for classifying spam messages from ham messages."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              ghLink="https://github.com/Kshitij-Agrawal-MKG/"
             />
           </Col>
 
@@ -82,8 +76,7 @@ function Projects() {
               isBlog={false}
               title="Speech Summarizer"
               description="This tool is designed to transcribe and summarize speech or audio files efficiently."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink="https://github.com/Kshitij-Agrawal-MKG/speech-summarizer" 
             />
           </Col>
         </Row>
